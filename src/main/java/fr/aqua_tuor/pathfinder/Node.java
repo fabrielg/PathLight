279b5534503c853d81b2120362164b44fa27dadc
Gabriel François <gfrancois2003@gmail.com>
2023-07-09 11:18:14 +0200

    feat: add show and active Node methods

--- src/main/java/fr/aqua_tuor/pathfinder/Node.java
+++ src/main/java/fr/aqua_tuor/pathfinder/Node.java
@@ -1,20 +1,47 @@
 package fr.aqua_tuor.pathfinder;
 
+import org.bukkit.Color;
+import org.bukkit.Particle;
+import org.bukkit.World;
+
 public class Node {
     private final int id;
     private final int x;
     private final int y;
     private final int z;
+    private final World world;
     private boolean isShow;
     private boolean isActive;
 
-    public Node(int id, int x, int y, int z) {
+    public Node(int id, int x, int y, int z, World world) {
         this.id = id;
         this.x = x;
         this.y = y;
         this.z = z;
+        this.world = world;
         this.isShow = false;
         this.isActive = false;
+    }
+
+    public void show(boolean toShow) {
+        if (toShow != isShow) isShow = toShow;
+
+        if (isShow) {
+            world.spawnParticle(Particle.REDSTONE, x, y, z, 0, 0, 0, 0, 1, new Particle.DustOptions(Color.RED, 1));
+        }
+    }
+
+    public void active(boolean toActive) {
+        if (!isShow) {
+            isActive = false;
+            return;
+        }
+
+        if (toActive != isActive) isActive = toActive;
+
+        if (isActive) {
+            world.spawnParticle(Particle.REDSTONE, x, y, z, 0, 0, 0, 0, 1, new Particle.DustOptions(Color.GREEN, 1));
+        }
     }
 
     public int getId() {
@@ -33,19 +60,15 @@ public class Node {
         return z;
     }
 
+    public World getWorld() {
+        return world;
+    }
+
     public boolean isShow() {
         return isShow;
     }
 
-    public void setShow(boolean isShow) {
-        this.isShow = isShow;
-    }
-
     public boolean isActive() {
         return isActive;
-    }
-
-    public void setActive(boolean isActive) {
-        this.isActive = isActive;
     }
 }
