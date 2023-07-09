f5ca482835e302f03807e506a917be6f9e6fc216
Gabriel François <gfrancois2003@gmail.com>
2023-07-09 10:40:25 +0200

    feat: showPath Line

--- src/main/java/fr/aqua_tuor/pathfinder/Path.java
+++ src/main/java/fr/aqua_tuor/pathfinder/Path.java
@@ -2,13 +2,14 @@ package fr.aqua_tuor.pathfinder;
 
 import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Particle;
 
 public class Path {
 
-    private Location start;
-    private Location end;
-    private PathType type;
-    private Color color;
+    private final Location start;
+    private final Location end;
+    private final PathType type;
+    private final Color color;
 
     public Path(Location start, Location end, PathType type, Color color) {
         this.start = start;
@@ -31,6 +32,44 @@ public class Path {
 
     public Color getColor() {
         return color;
+    }
+
+    public void drawPath() {
+        switch (type) {
+            case LINE:
+                drawLine();
+                break;
+            case ARC:
+            case AROUND:
+            case SNAKE:
+                break;
+        }
+    }
+
+    public void drawLine() {
+        double distance = getDistance();
+        double x = start.getX();
+        double y = start.getY();
+        double z = start.getZ();
+        double x2 = end.getX();
+        double y2 = end.getY();
+        double z2 = end.getZ();
+        double deltaX = (x2 - x) / distance;
+        double deltaY = (y2 - y) / distance;
+        double deltaZ = (z2 - z) / distance;
+
+        for (int i = 0; i < distance; i++) {
+            x += deltaX;
+            y += deltaY;
+            z += deltaZ;
+            start.getWorld().spawnParticle(org.bukkit.Particle.REDSTONE, x, y, z, 0, 0, 0, 0, 1, new Particle.DustOptions(color, 1));
+        }
+
+    }
+
+
+    public double getDistance() {
+        return start.distance(end);
     }
 
 }
