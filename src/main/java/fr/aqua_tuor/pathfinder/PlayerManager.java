c3f0b2e3cc696958ce79c107fdb9b35efc97e5c4
Gabriel François <gfrancois2003@gmail.com>
2023-07-09 11:39:33 +0200

    feat: add methods editor PlayerManager

--- src/main/java/fr/aqua_tuor/pathfinder/PlayerManager.java
+++ src/main/java/fr/aqua_tuor/pathfinder/PlayerManager.java
@@ -1,11 +1,37 @@
 package fr.aqua_tuor.pathfinder;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PlayerManager {
 
     private PathManager pathManager;
 
+    HashMap<Player, Inventory> playersEditing = new HashMap<>();
+
     public PlayerManager(PathManager pathManager) {
         this.pathManager = pathManager;
+    }
+
+    public void giveEditorTools(Player player) {
+    }
+
+    public void addPlayer(Player player) {
+        if (!playersEditing.containsValue(player)) {
+            playersEditing.put(player, player.getInventory());
+            giveEditorTools(player);
+        }
+    }
+
+    public void removePlayer(Player player) {
+        if (playersEditing.containsValue(player)) {
+            player.getInventory().clear();
+            player.getInventory().setContents(playersEditing.get(player).getContents());
+            playersEditing.remove(player);
+        }
     }
 
 }
