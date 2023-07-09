2f78cf152e0ab3b7ebf400e442ceb691d71728fc
Gabriel François <gfrancois2003@gmail.com>
2023-07-09 11:32:29 +0200

    feat: init PlayerManager

--- src/main/java/fr/aqua_tuor/pathfinder/PathManager.java
+++ src/main/java/fr/aqua_tuor/pathfinder/PathManager.java
@@ -7,14 +7,16 @@ import java.util.ArrayList;
 public class PathManager {
 
     private final PathFinder plugin;
+    private PlayerManager playerManager;
 
     private ArrayList<Path> paths;
-    private ArrayList<Node> nodes;
+    private ArrayList<Node> nodes = new ArrayList<>();
 
     public PathManager(PathFinder plugin) {
         this.plugin = plugin;
+        this.playerManager = new PlayerManager(this);
         paths = new ArrayList<>();
-        nodes = new ArrayList<>();
+        loadNodesConfig();
     }
 
     public PathFinder getPlugin() {
@@ -33,6 +35,10 @@ public class PathManager {
             Node node = new Node(id, x, y, z, Bukkit.getWorld(worldName));
             nodes.add(node);
         });
+    }
+
+    public PlayerManager getPlayerManager() {
+        return playerManager;
     }
 
 }
---
+++ src/main/java/fr/aqua_tuor/pathfinder/PlayerManager.java
@@ -0,0 +1,11 @@
+package fr.aqua_tuor.pathfinder;
+
+public class PlayerManager {
+
+    private PathManager pathManager;
+
+    public PlayerManager(PathManager pathManager) {
+        this.pathManager = pathManager;
+    }
+
+}
