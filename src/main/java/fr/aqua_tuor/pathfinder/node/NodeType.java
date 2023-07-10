5d9758c5269f01066ba2ac5759ba66c0b6361c83
Gabriel François <gfrancois2003@gmail.com>
2023-07-10 18:31:22 +0200

    fix: review SELECTED node particles

--- src/main/java/fr/aqua_tuor/pathfinder/node/NodeType.java
+++ src/main/java/fr/aqua_tuor/pathfinder/node/NodeType.java
@@ -6,7 +6,7 @@ import org.bukkit.Particle;
 public enum NodeType {
 
     NORMAL(new Particle.DustOptions(Color.RED, 1)),
-    SELECTED(new Particle.DustOptions(Color.GREEN, 2)),
+    SELECTED(new Particle.DustOptions(Color.LIME, 1.5f)),
     PREVIEW(new Particle.DustOptions(Color.BLACK, 1));
 
     private Particle.DustOptions dustOptions;
